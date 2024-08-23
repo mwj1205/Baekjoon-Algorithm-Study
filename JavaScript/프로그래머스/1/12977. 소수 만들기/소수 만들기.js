f16309582fcb90e75e6928689a147028917b31f1
Minu Han <75732781+mwj1205@users.noreply.github.com>
2024-08-23 09:26:41 +0900

    [level 1] Title: 소수 만들기, Time: 4.55 ms, Memory: 35.9 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -0,0 +1,21 @@
+function isPrime(num) {
+    for(let i = 2; i <= Math.sqrt(num); i++) {
+        if(num % i === 0) return false
+    }
+    return true;
+}
+
+function solution(nums) {
+    var answer = 0;
+
+    for(let i = 0; i < nums.length - 2; i++) {
+        for(let j = i+1; j < nums.length - 1; j++) {
+            for(let k = j+1; k < nums.length; k++) {
+                let temp = nums[i] + nums[j] + nums[k];
+                if(isPrime(temp)) answer++;
+            }
+        }
+    }
+    return answer;
+}
+
