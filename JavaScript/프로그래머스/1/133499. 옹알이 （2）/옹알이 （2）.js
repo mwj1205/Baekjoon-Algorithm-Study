28bd700cfa78189950136e7cf53e86f4f03dfe65
Minu Han <75732781+mwj1205@users.noreply.github.com>
2024-08-27 16:47:40 +0900

    [level 1] Title: 옹알이 (2), Time: 0.86 ms, Memory: 33.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
+++ JavaScript/프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
@@ -1,14 +1,4 @@
 function solution(babbling) {
-    const cans = ['aya', 'ye', 'woo', 'ma'];
-    let answer = 0;
-    
-    for(let bob of babbling) {
-        for(let can of cans) {
-            if(bob.includes(can.repeat(2))) break;
-            bob = bob.split(can).join(" ");
-        }
-        if(bob.split(" ").join("").length === 0) answer += 1;
-    }
-    
+    var answer = 0;
     return answer;
 }
