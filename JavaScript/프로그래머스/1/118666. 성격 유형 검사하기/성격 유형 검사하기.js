d5d7a4e21abbfb7c952b1f35413acab70d1a877d
Minu Han <75732781+mwj1205@users.noreply.github.com>
2024-08-30 09:52:43 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.81 ms, Memory: 33.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ JavaScript/프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,21 +1,10 @@
 function solution(survey, choices) {
-    var answer = '';
     let kkti = {R: 0, T: 0, C: 0, F: 0, J: 0, M: 0, A: 0, N: 0};
+    const types = ['RT', 'CF', 'JM', 'AN'];
     for(let i = 0; i < survey.length; i++) {
-        if(choices[i] < 4) {
-            kkti[survey[i][0]] += 4 - choices[i];
-        } 
-        else if(choices[i] > 4) {
-            kkti[survey[i][1]] += choices[i] - 4;
-        }
+        const [disagree, agree] = survey[i];
+        if(choices[i] < 4) kkti[disagree] += 4 - choices[i];
+        else if(choices[i] > 4)  kkti[agree] += choices[i] - 4;
     }
-    if(kkti.R >= kkti.T) answer += 'R';
-    else answer += 'T';
-    if(kkti.C >= kkti.F) answer += 'C';
-    else answer += 'F';
-    if(kkti.J >= kkti.M) answer += 'J';
-    else answer += 'M';
-    if(kkti.A >= kkti.N) answer += 'A';
-    else answer += 'N';
-    return answer;
+    return types.map(([a, b]) => kkti[b] > kkti[a] ? b : a).join("");
 }
