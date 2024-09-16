7d344363b0c63be0958cd2e355e766d6d147e561
Minu Han <75732781+mwj1205@users.noreply.github.com>
2024-09-16 17:14:59 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 86.23 ms, Memory: 57 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ JavaScript/프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,15 +1,13 @@
 function solution(elements) {
     const sums = new Set();
     
-    for(let k = 1; k <= elements.length; k++) {
-        for(let i = 0; i < elements.length; i++) {
-            let sum = 0;
-            for(let j = 0; j < k; j++) {
-                sum += elements[(j + i) % elements.length];
-            }
+    for(let i = 0; i < elements.length; i++) {
+        let sum = 0;
+        for(let j = 0; j < elements.length; j++) {
+            sum += elements[(j + i) % elements.length];
             sums.add(sum);
-            if(k === elements.length) break;
         }
+
     }
     return sums.size;
 }
