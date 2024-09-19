235e41ed20ef3d0bea9c1568c741b185ce564f49
Minu Han <75732781+mwj1205@users.noreply.github.com>
2024-09-19 10:05:59 +0900

    [level 2] Title: n^2 배열 자르기, Time: 8.14 ms, Memory: 46.7 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
+++ JavaScript/프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
@@ -1,15 +1,10 @@
 function solution(n, left, right) {
     var answer = [];
     let lrow = Math.floor(left / n) + 1;
-
     let lcol = left % n + 1;
+    
     for(let i = left; i <= right; i++) {
-        answer.push(Math.max(lrow, lcol));
-        lcol++;
-        if(lcol > n) {
-            lcol = 1;
-            lrow++;
-        }
+        answer.push(Math.max(Math.floor(i / n) + 1, i % n + 1));
     }
     return answer;
 }
