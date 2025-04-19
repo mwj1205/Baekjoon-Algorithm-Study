d551c82937967257321e7f6fb89da97c85429c35
Minu Han <75732781+mwj1205@users.noreply.github.com>
2025-04-19 12:28:35 +0900

    [level 2] Title: 짝지어 제거하기, Time: 58.80 ms, Memory: 50.9 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,17 @@
+function solution(s)
+{
+    const stack = [];
+    
+    for (const char of s) {
+        if (stack.length > 0 && stack[stack.length - 1] === char) {
+            stack.pop();
+        } else {
+            stack.push(char);
+        }
+    }
+    if (stack.length > 0) {
+        return 0;
+    } else {
+        return 1;
+    }
+}
