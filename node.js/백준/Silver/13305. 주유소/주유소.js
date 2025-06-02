315575c8c72b72c4f6812ab989f5f27514d78f2e
Minu Han <75732781+mwj1205@users.noreply.github.com>
2025-06-02 14:34:58 +0900

    [Silver III] Title: 주유소, Time: 272 ms, Memory: 44784 KB, Score: 100 point -BaekjoonHub

--- node.js/백준/Silver/13305. 주유소/주유소.js
+++ node.js/백준/Silver/13305. 주유소/주유소.js
@@ -2,11 +2,11 @@ const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const N = Number(input[0]);
-const roads = input[1].split(' ').map(Number);
-const prices = input[2].split(' ').map(Number);
+const roads = input[1].split(' ').map(BigInt);
+const prices = input[2].split(' ').map(BigInt);
 
+let totalCost = 0n;
 let minPrice = prices[0];
-let totalCost = 0;
 
 for (let i = 0; i < N - 1; i++) {
   if (prices[i] < minPrice) {
@@ -15,4 +15,4 @@ for (let i = 0; i < N - 1; i++) {
   totalCost += minPrice * roads[i];
 }
 
-console.log(totalCost);
+console.log(totalCost.toString());
